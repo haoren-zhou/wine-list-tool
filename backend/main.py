a93b2bd4cb8600d45d0160385199a350b130a107
sleepingenthusiast <104418647+sleepingenthusiast@users.noreply.github.com>
2025-04-09 17:25:19 +0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,7 @@
-from typing import Union
-
+from typing import Union, BinaryIO
 from fastapi import FastAPI, UploadFile
-
 from pypdf import PdfReader
+import asyncio
 
 app = FastAPI()
 
@@ -11,27 +10,27 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q+q}
-
-@app.post("/upload")
-def parse_pdf(file: UploadFile | None = None):
-    if not file:
-        return {"message": "No upload file sent"}
-    
-    pdf = PdfReader(file.file)
-    # Extract PDF text
-    text = ""
-    for page_number in range(len(pdf.pages)):
-        page = pdf.pages[page_number]
-        text += page.extract_text().strip()
-        text += "\n(End of Page)\n"
-    text += "End of Document"
-
+def extract_pdf_text_sync(pdf: BinaryIO) -> str:
+    reader = PdfReader(pdf)
+    text = "\n(End of Page)\n".join(page.extract_text().strip() or "" for page in reader.pages)
+    text += "(End of Document)"
     # Clean up weird apostrophes
     text = text.replace("“", '"')
     text = text.replace("”", '"')
     text = text.replace("‘", "'")
     text = text.replace("’", "'")
+    return text
+
+async def extract_pdf_text(pdf: BinaryIO) -> str:
+    return await asyncio.to_thread(extract_pdf_text_sync, pdf)
+
+def extract_wine_info(text: str) -> list:
+    
+
+@app.post("/upload")
+async def parse_pdf(file: UploadFile | None = None):
+    if not file:
+        return {"message": "No upload file sent"}
+    
+    text = await extract_pdf_text(file.file)
     return {"filename": file.filename, "text": text}
