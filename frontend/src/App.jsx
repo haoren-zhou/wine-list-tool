61582b65be9ed95065ef1cf38fcddbb2505ccd4e
sleepingenthusiast <104418647+sleepingenthusiast@users.noreply.github.com>
2025-04-19 01:09:48 +0800

    Major implementations
    1. PDFFormSection for document upload
    2. Improved CSS for styling
    3. Updated WineCard for JSON API output format

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,104 +1,122 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
+import { useState } from 'react';
+// import './App.css';
+import PDFFormSection from './FormPage';
+import { Routes, Route, Link } from 'react-router-dom';
 
-import { Routes, Route, Link } from 'react-router-dom'
+export const FileStatus = Object.freeze({
+  IDLE: 'IDLE', // File not uploaded yet (initial state)
+  PROCESSING: 'PROCESSING', // File is being processed by the backend
+  SUCCESS: 'SUCCESS', // Backend processing finished successfully
+  ERROR: 'ERROR', // An error occurred during upload or processing
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [fileStatus, setFileStatus] = useState(FileStatus.SUCCESS);
+  const [wineList, setWineList] = useState([]);
+  const renderContent = () => {
+    switch (fileStatus) {
+      case FileStatus.PROCESSING:
+        return <p>Processing...</p>;
+      case FileStatus.SUCCESS:
+        return <FilterableWineList winelist={WINELIST} />;
+      case FileStatus.ERROR:
+        return(
+          <>
+            <p>Error occurred during file processing.</p>
+            <button onClick={() => setFileStatus(FileStatus.IDLE)}>Back</button>
+          </>
+        )
+      case FileStatus.IDLE:
+      default:
+        return <PDFFormSection setFileStatus={setFileStatus} setWineList={setWineList}/>;
+    }
+  }
   return (
     <>
-      <header>
-        <p>----header text----</p>
+      <header className='flex bg-gray-800 place-content-center items-center h-16 md:h-20 lg:h-24 xl:h-28'>
+        <h1 className='text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl text-transparent bg-clip-text bg-gradient-to-r to-yellow-100 from-red-700'>Wine List Tool</h1>
       </header>
-      <Routes>
-        <Route path="/" element={<b>test test</b>} />
-      </Routes>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR hello yes
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-      <FilterableWineList winelist={WINELIST}/>
+      <main className="w-9/10 md:w-4/5 xl:w-7/10 place-self-center">
+        {/* <Routes>
+          <Route path="/" element={<b>test test</b>} />
+        </Routes> */}
+        {renderContent()}
+      </main>
       <footer>
         <p>----footer text----</p>
       </footer>
     </>
-  )
+  );
 }
 
 function Filters () {
+  // TODO: implement filters
+  // 1. select by wine type
+  // 2. Filter by rating (minimum rating)
+  // 3. Filter by price (maximum price)
+  // 4. Filter by format (750ml, 150ml, etc.)
+  // 5. Sort by (rating, price)
   return (
     <form>
       <button>placeholder</button>
     </form>
-  )
+  );
 }
 
 function WineList({ winelist }) {
-  const [activeIndex, setActiveIndex] = useState(-1) // -1 if no card is open
+  const [activeIndex, setActiveIndex] = useState(-1); // -1 if no card is open
   return (
-    <div>
+    <div className='space-y-4'>
       {winelist.map((wine, index) => {
-        return <WineCard 
-          wine={wine}
-          isActive={activeIndex === index}
-          onToggle={() => setActiveIndex(activeIndex === index? -1 : index)}
-          key={index}
-        />
+        return (
+          <WineCard 
+            wine={wine}
+            isActive={activeIndex === index}
+            onToggle={() => setActiveIndex(activeIndex === index? -1 : index)}
+            key={index}
+          />
+        );
       })}
     </div>
-  )
+  );
 }
 
 function WineCard({ wine, isActive, onToggle }) {
   return (
     <>
-      <details open={isActive} className='border rounded-sm m-4 marker:content-none'>
-        <summary className='p-2 bg-gray-900 text-lg' onClick={(e) => {
+      <details open={isActive} className='rounded-md marker:content-none'>
+        <summary className='p-4 bg-gray-700 cursor-pointer' onClick={(e) => {
           e.preventDefault();
           onToggle();
         }}>
-          {wine.name}
+          <div className='flex items-center justify-between text-white'>
+            <div className='font-semibold text-sm md:text-base xl:text-lg 2xl:text-xl'>{wine.vivino_match}</div>
+            <div className='font-medium text-right text-xs md:text-sm xl:text-base 2xl:text-lg'>{wine.rating_average} &#9733; ({wine.rating_count} reviews) &middot; {wine.type_name} &middot; ${wine.price}</div>
+          </div>
         </summary>
-        <div className='p-2 bg-gray-800'>
-          <p>
-            Vintage: {wine.vintage}
-          </p>
-          <p>
-            Price: {wine.price} SGD
-          </p>
+        <div className='p-4 bg-gray-600 text-gray-200 text-xs md:text-sm xl:text-base 2xl:text-lg'>
+          <p><strong>Name on Wine List:</strong> {wine.wine_name} {wine.vintage}</p>
+          <p><strong>Style:</strong> {wine.style_name}</p>
+          <p><strong>Grapes:</strong> {wine.grapes_name}</p>
+          <p><strong>Volume:</strong> {wine.volume}</p>
         </div>
       </details>
     </>
-  )
+  );
 }
 
 const WINELIST = [
-  {"key": 0, "name": "wine 1 name", "vintage": "2022", "price": 458},
-  {"key": 1, "name": "wine 2 name aaa", "vintage": "N.V.", "price": 38},
-  {"key": 2, "name": "wine 3 asdiong", "vintage": "2019", "price": 98},
-  {"key": 3, "name": "wine4 test", "vintage": "2018", "price": 409},
-  {"key": 4, "name": "wine 5", "vintage": "1996", "price": 240},
-  {"key": 5, "name": "wine 6 llll'or", "vintage": "2014", "price": 270},
-]
+  {'wine_name': 'Champagne Egly-Ouriet, Les Premices, Extra Brut', 'vivino_match': "Egly-Ouriet Les Prémices Brut Champagne Grand Cru 'Ambonnay'", 'rating_average': 4.2, 'rating_count': 2036, 'type_id': 3, 'style_id': 50, 'grapes': [5], 'vintage': 'N.V.', 'price': 50, 'volume': 150, 'type_name': 'Sparkling', 'style_name': 'French Champagne', 'grapes_name': 'Chardonnay'}, 
+  {'wine_name': 'Champagne Marguet, Shaman 21, Rosé, Brut Nature', 'vivino_match': 'Dom Pérignon Brut Rosé Champagne', 'rating_average': 4.6, 'rating_count': 15882, 'type_id': 3, 'style_id': 50, 'grapes': [14], 'vintage': 'N.V.', 'price': 60, 'volume': 150, 'type_name': 'Sparkling', 'style_name': 'French Champagne', 'grapes_name': 'Pinot Noir'}, 
+  {'wine_name': 'Champagne Dom Pérignon, Brut', 'vivino_match': 'Dom Pérignon Brut Champagne 2015', 'rating_average': 4.4, 'rating_count': 1797, 'type_id': 3, 'style_id': 50, 'grapes': [5, 14], 'vintage': 2015, 'price': 100, 'volume': 150, 'type_name': 'Sparkling', 'style_name': 'French Champagne', 'grapes_name': 'Chardonnay, Pinot Noir'}, 
+  {'wine_name': 'Champagne Pierre Péters, Cuvée de Réserve, Brut, Blanc de blancs', 'vivino_match': "Pierre Peters Cuvée de Réserve Blanc de Blancs Brut Champagne Grand Cru 'Le Mesnil-sur-Oger'", 'rating_average': 4.1, 'rating_count': 10588, 'type_id': 3, 'style_id': 50, 'grapes': [5], 'vintage': 'N.V.', 'price': 65, 'volume': 150, 'type_name': 'Sparkling', 'style_name': 'French Champagne', 'grapes_name': 'Chardonnay'}, 
+  {'wine_name': 'Jim Bary, Clare Valley, Australia, Assyrtiko', 'vivino_match': 'Jim Barry Assyrtiko 2023', 'rating_average': 3.8, 'rating_count': 127, 'type_id': 2, 'style_id': null, 'grapes': [259], 'vintage': 2023, 'price': 35, 'volume': 150, 'type_name': 'White', 'style_name': 'N.A.', 'grapes_name': 'Assyrtiko'}, 
+  {'wine_name': 'Château des Vaults, Domaine du Closel, La Jalousie, Savennières', 'vivino_match': 'Domaine du Closel - Château des Vaults La Jalousie Savennières 2021', 'rating_average': 4.1, 'rating_count': 26, 'type_id': 2, 'style_id': 251, 'grapes': [6], 'vintage': 2021, 'price': 40, 'volume': 150, 'type_name': 'White', 'style_name': 'Loire Chenin Blanc', 'grapes_name': 'Chenin Blanc'}, 
+  {'wine_name': 'Domaine Jean Fournier, Clos du Roy, Marsannay', 'vivino_match': "Jean Fournier Marsannay 'Clos du Roy' Rouge 2020", 'rating_average': 4.1, 'rating_count': 116, 'type_id': 1, 'style_id': 283, 'grapes': [14], 'vintage': 2020, 'price': 60, 'volume': 150, 'type_name': 'Red', 'style_name': 'Burgundy Côte de Nuits Red', 'grapes_name': 'Pinot Noir'}, 
+  {'wine_name': "Château d'Esclans, Garrus, Côte de Provence, France", 'vivino_match': "Château d'Esclans Garrus Rosé 2022", 'rating_average': 4.4, 'rating_count': 98, 'type_id': 4, 'style_id': 173, 'grapes': [2, 56, 1], 'vintage': 2022, 'price': 55, 'volume': 150, 'type_name': 'Rosé', 'style_name': 'Provence Rosé', 'grapes_name': 'Cabernet Sauvignon, Grenache Blanc, Shiraz/Syrah'}, 
+  {'wine_name': 'Château de Beaucastel, Châteauneuf-du-Pape', 'vivino_match': 'Château de Beaucastel Châteauneuf-du-Pape 1995', 'rating_average': 4.3, 'rating_count': 815, 'type_id': 1, 'style_id': 301, 'grapes': [8], 'vintage': 1995, 'price': 680, 'volume': 750, 'type_name': 'Red', 'style_name': 'Southern Rhône Châteauneuf-du-Pape Red', 'grapes_name': 'Grenache'}, 
+  {'wine_name': 'Querciabella, Chianti Classico', 'vivino_match': 'Querciabella Chianti Classico 1998', 'rating_average': 4.2, 'rating_count': 98, 'type_id': 1, 'style_id': 887, 'grapes': [16], 'vintage': 1998, 'price': 60, 'volume': 150, 'type_name': 'Red', 'style_name': 'Italian Chianti Classico Red', 'grapes_name': 'Sangiovese'}
+];
 
 function FilterableWineList ({ winelist }) {
   return (
@@ -107,12 +125,8 @@ function FilterableWineList ({ winelist }) {
       <WineList winelist={winelist}/>
     </div>
   )
-}
+};
 
 
 
-
-
-
-
-export default App
+export default App;
---
+++ frontend/src/FormPage.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { FileStatus } from './App';
+
+function PDFFormSection({setFileStatus, setWineList}) {
+    // const [isLoading, setIsLoading] = useState(false);
+
+    const handleFileChange = async (event) => {
+        const file = event.target.files[0];
+        if (!file) return;
+        if (file.type !== 'application/pdf') {
+            alert('Please upload a PDF file.');
+            return;
+        }
+        if (file.size > 10 * 1024 * 1024) { // 10MB limit
+            alert('File size exceeds 10MB limit.');
+            return;
+        }
+        setFileStatus(FileStatus.PROCESSING);
+        console.log(`Uploading file: ${file.name}`);
+
+        const formData = new FormData();
+        formData.append('file', file);
+
+        const response = await fetch('http://localhost:8000/upload', {
+            method: 'POST',
+            body: formData,
+        });
+        if (response.ok) {
+            console.log('File uploaded successfully!');
+            setFileStatus(FileStatus.SUCCESS);
+        } else {
+            // console.log(`Error: ${result.message}`);
+            setFileStatus(FileStatus.ERROR);
+        }
+        const result = await response.json();
+        setWineList(result.wine_details);
+        console.log(result);
+        return;
+    }
+
+    return (
+        <form>        
+            <div className="flex items-center justify-center w-full place-self-center">
+                <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-96 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                        <svg className="w-16 h-16 mb-4 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                        </svg>
+                        <p className="mb-2 text-lg text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                        <p className="text-base text-gray-400">PDF only (MAX. 10MB)</p>
+                    </div>
+                    <input 
+                        id="dropzone-file" 
+                        type="file" 
+                        accept=".pdf" 
+                        className="hidden" 
+                        onChange={handleFileChange}
+                    />
+                </label>
+            </div> 
+        </form>
+    );
+}
+
+export default PDFFormSection;
