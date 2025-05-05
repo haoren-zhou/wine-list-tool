ee47072d8f6b7003897edaaa1aa55f9fe9ddeef0
haoren-zhou <104418647+haoren-zhou@users.noreply.github.com>
2025-05-06 00:33:22 +0800

    Implement thresholds and filters functionality
    TODO: sort by

--- frontend/src/FilterableWineList.jsx
+++ frontend/src/FilterableWineList.jsx
@@ -1,42 +1,51 @@
 import React, { useState } from 'react';
 
-function Filters () {
+function Filters ({winelist, minRating, setMinRating, maxPrice, setMaxPrice, setTypeFilter, setFormatFilter}) {
     // TODO: implement filters
     // 1. Filter by rating (minimum rating) -> useState (min_rating float) ratingThreshold
     // 2. Filter by price (maximum price) -> useState (max_price int) priceThreshold
     // 3. select by wine type -> useState (All, ...types) typeFilter
     // 4. Filter by format (750ml, 150ml, etc.) -> useState (All, ...formats) formatFilter
     // 5. Sort by (rating, price) sortBy -> priceAsc, priceDesc, ratingAsc, ratingDesc
-    const [minRating, setMinRating] = useState(4.0);
-    const [maxPrice, setMaxPrice] = useState(1500);
+    
+    const wineTypes = [...new Set(winelist.map(wine => wine.type_name))];
+    const formats = [...new Set(winelist.map(wine => wine.volume))];
     return (
-      <div className='text-white width-full mb-4 grid grid-rows-2 grid-cols-12 gap-2'>
+      <div className='text-white width-full mb-4 grid grid-rows-2 grid-cols-12 gap-x-4'>
         <div className='col-span-full md:col-span-6'>
-            <label htmlFor='ratingThreshold'>Min. Rating</label>
+            <label className="font-semibold" htmlFor='ratingThreshold'>Min. Rating</label>
             <span className='float-right'>{Number(minRating).toFixed(1)}</span>
             <input type="range" min="3.0" max="5.0" defaultValue="4.0" step="0.1" id='ratingThreshold' className='w-full' onInput={(e) => {setMinRating(e.target.value)}} />
         </div>
         <div className='col-span-full md:col-span-6'>
-            <label htmlFor='priceThreshold'>Max. Price ($)</label>
+            <label className="font-semibold" htmlFor='priceThreshold'>Max. Price ($)</label>
             <span className='float-right'>{maxPrice == 999999 ? "-" : maxPrice}</span>
             <input type="range" min="0" max="3005" defaultValue="1500" step="5" id='priceThreshold' className='w-full' onInput={(e) => {e.target.value == 3005 ? setMaxPrice(999999) : setMaxPrice(e.target.value)}} />
         </div>
         <div className='col-span-full md:col-span-4'>
-          <label htmlFor='typeFilter'>Wine Type</label>
-          <select className="w-full bg-gray-800 px-4 py-2.5 pr-8 rounded leading-tight" id='typeFilter'>
-            <option>test</option>
-            <option>asdg</option>
+          <label className="font-semibold" htmlFor='typeFilter'>Wine Type</label>
+          <select className="w-full bg-gray-800 px-4 py-2.5 pr-8 rounded leading-tight" id='typeFilter' defaultValue="" onChange={(e) => {setTypeFilter(e.target.value)}}>
+            <option value="">All</option>
+            {
+              wineTypes.map((type, index) => 
+                <option key={index}>{type}</option>
+              )
+            }
           </select>
         </div>
         <div className='col-span-full md:col-span-4'>
-          <label htmlFor='formatFilter'>Format</label>
-          <select className="w-full bg-gray-800 px-4 py-2.5 pr-8 rounded leading-tight" id='formatFilter'>
-            <option>test</option>
-            <option>asdg</option>
+          <label className="font-semibold" htmlFor='formatFilter'>Format</label>
+          <select className="w-full bg-gray-800 px-4 py-2.5 pr-8 rounded leading-tight" id='formatFilter' defaultValue="0" onChange={(e) => {setFormatFilter(Number(e.target.value))}}>
+          <option value={0}>All</option>
+            {
+              formats.map((format, index) => 
+                <option key={index} value={format}>{format} ml</option>
+              )
+            }
           </select>
         </div>
         <div className='col-span-full md:col-span-4'>
-          <label htmlFor='sortByFilter'>Sort By</label>
+          <label className="font-semibold" htmlFor='sortByFilter'>Sort By</label>
           <select className="w-full bg-gray-800 px-4 py-2.5 pr-8 rounded leading-tight" id='sortByFilter'>
             <option>test</option>
             <option>asdg</option>
@@ -46,11 +55,17 @@ function Filters () {
     );
   }
   
-  function WineList({ winelist }) {
+  function WineList({ winelist, minRating, maxPrice, typeFilter, formatFilter }) {
     const [activeIndex, setActiveIndex] = useState(-1); // -1 if no card is open
+    function checkFilters(wineDetails) {
+      return (typeFilter ? wineDetails.type_name == typeFilter : true) && (formatFilter ? wineDetails.volume == formatFilter : true);
+    }
+    const filteredWinelist = winelist.filter(
+      wineDetails => wineDetails.rating_average >= minRating && wineDetails.price <= maxPrice && checkFilters(wineDetails)
+    );
     return (
       <div className='space-y-4'>
-        {winelist.map((wine, index) => {
+        {filteredWinelist.map((wine, index) => {
           return (
             <WineCard 
               wine={wine}
@@ -89,10 +104,14 @@ function Filters () {
   }
   
   function FilterableWineList ({ winelist }) {
+    const [minRating, setMinRating] = useState(4.0);
+    const [maxPrice, setMaxPrice] = useState(1500);
+    const [typeFilter, setTypeFilter] = useState("");
+    const [formatFilter, setFormatFilter] = useState(0);
     return (
       <div>
-        <Filters />
-        <WineList winelist={winelist}/>
+        <Filters winelist={winelist} {...{minRating, setMinRating, maxPrice, setMaxPrice, setTypeFilter, setFormatFilter}}/>
+        <WineList winelist={winelist} {...{minRating, maxPrice, typeFilter, formatFilter}}/>
       </div>
     )
   };
