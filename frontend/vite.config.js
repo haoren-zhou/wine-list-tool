25f8908472307aba1d159a52af32f4b296225f17
sleepingenthusiast <104418647+sleepingenthusiast@users.noreply.github.com>
2025-06-29 00:41:14 +0800

    build(deps): update deps and add autoprefixer

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import autoprefixer from "autoprefixer";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,11 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer
+      ],
+    },
+  },
 })
